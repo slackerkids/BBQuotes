c35aa82c028315a8da6a44c6686c7cbbb0ae1c44
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-12-08 18:06:24 +0500

    Initial Commit

---
+++ BBQuotes18/BBQuotes18App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes18App.swift
+//  BBQuotes18
+//
+//  Created by Sagidanov Bakhtiyar on 08.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBQuotes18/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BBQuotes18
+//
+//  Created by Sagidanov Bakhtiyar on 08.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
