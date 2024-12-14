ff7437923ac3b6425f87293d9cb82692635789ba
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-12-14 12:01:31 +0500

    Networking function start

---
+++ BBQuotes18/FetchService.swift
@@ -0,0 +1,32 @@
+//
+//  FetchService.swift
+//  BBQuotes18
+//
+//  Created by Sagidanov Bakhtiyar on 14.12.2024.
+//
+
+import Foundation
+
+struct FetchService {
+    enum FetchError: Error {
+        case badResponse
+    }
+    
+    let baseURL = URL(string: "https://breaking-bad-api-six.vercel.app/api")!
+    
+    // https://breaking-bad-api-six.vercel.app/api/quotes/random?production=Breaking+Bad
+    func fetchQuote(from show: String) async throws -> Quote {
+        // Build fetch url
+        let quoteURL = baseURL.appending(path: "quotes/random")
+        let fetchURL = quoteURL.appending(queryItems: [URLQueryItem(name: "production", value: show)])
+        
+        // Fetch data
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        // Handle response
+        
+        // Decode data
+        
+        // Return quote
+    }
+}
