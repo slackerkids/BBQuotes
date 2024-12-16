64bc5dad488aa3e39f77d9d73ce25f77c6a62c68
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-12-16 11:15:55 +0500

    ViewModel start

--- BBQuotes18/FetchService.swift
+++ BBQuotes18/FetchService.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 struct FetchService {
-    enum FetchError: Error {
+    private enum FetchError: Error {
         case badResponse
     }
     
-    let baseURL = URL(string: "https://breaking-bad-api-six.vercel.app/api")!
+    private let baseURL = URL(string: "https://breaking-bad-api-six.vercel.app/api")!
     
     // https://breaking-bad-api-six.vercel.app/api/quotes/random?production=Breaking+Bad
     func fetchQuote(from show: String) async throws -> Quote {
@@ -24,9 +24,55 @@ struct FetchService {
         let (data, response) = try await URLSession.shared.data(from: fetchURL)
         
         // Handle response
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
         
         // Decode data
+        let quote = try JSONDecoder().decode(Quote.self, from: data)
         
         // Return quote
+        return quote
+    }
+    
+    func fetchCharachter(_ name: String) async throws -> Character {
+        let characterURL = baseURL.appending(path: "characters")
+        let fetchURL = characterURL.appending(queryItems: [URLQueryItem(name: "name", value: name)])
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let characters = try decoder.decode([Character].self, from: data)
+        
+        return characters[0]
+    }
+    
+    func fetchDeath(for character: String) async throws -> Death? {
+        let fetchURL = baseURL.appending(path: "death")
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let deaths = try decoder.decode([Death].self, from: data)
+        
+        for death in deaths {
+            if death.character == character {
+                return death
+            }
+        }
+        
+        return nil
     }
 }
---
+++ BBQuotes18/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  BBQuotes18
+//
+//  Created by Sagidanov Bakhtiyar on 15.12.2024.
+//
+
+import Foundation
+
+@Observable
+class ViewModel {
+    
+}
