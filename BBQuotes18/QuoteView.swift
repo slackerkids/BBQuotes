9ac05b31152537c2cdd50603138b383f41d80d6d
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-12-26 15:44:40 +0500

    Quote View

--- BBQuotes18/QuoteView.swift
+++ BBQuotes18/QuoteView.swift
@@ -20,48 +20,50 @@ struct QuoteView: View {
                     .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
                 
                 VStack {
-                    Spacer(minLength: 60)
-                    
-                    switch vm.status {
-                    case .notStarted:
-                        <#code#>
-                    case .fetching:
-                        <#code#>
-                    case .success:
-                        Text("\"\(vm.quote.quote)\"")
-                            .minimumScaleFactor(0.5)
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(.white)
-                            .padding()
-                            .background(.black.opacity(0.5))
-                            .clipShape(.rect(cornerRadius: 25))
-                            .padding(.horizontal)
+                    VStack {
+                        Spacer(minLength: 60)
                         
-                        ZStack(alignment: .bottom) {
-                            AsyncImage(url: vm.character.images[0]) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                            } placeholder: {
-                                ProgressView()
+                        switch vm.status {
+                        case .notStarted:
+                            EmptyView()
+                        case .fetching:
+                            ProgressView()
+                        case .success:
+                            Text("\"\(vm.quote.quote)\"")
+                                .minimumScaleFactor(0.5)
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.white)
+                                .padding()
+                                .background(.black.opacity(0.5))
+                                .clipShape(.rect(cornerRadius: 25))
+                                .padding(.horizontal)
+                            
+                            ZStack(alignment: .bottom) {
+                                AsyncImage(url: vm.character.images[0]) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: geo.size.width / 1.1, height: geo.size.height / 1.8)
+                                
+                                Text(vm.quote.character)
+                                    .foregroundStyle(.white)
+                                    .padding(10)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.ultraThinMaterial)
                             }
                             .frame(width: geo.size.width / 1.1, height: geo.size.height / 1.8)
-                            
-                            Text(vm.quote.character)
-                                .foregroundStyle(.white)
-                                .padding(10)
-                                .frame(maxWidth: .infinity)
-                                .background(.ultraThinMaterial)
+                            .clipShape(.rect(cornerRadius: 50))
+                        case .failed(let error):
+                            Text(error.localizedDescription)
                         }
-                        .frame(width: geo.size.width / 1.1, height: geo.size.height / 1.8)
-                        .clipShape(.rect(cornerRadius: 50))
-                    case .failed(let error):
-                        <#code#>
+
+                        
+                        
+                        Spacer()
                     }
-                    
-                    
-                    
-                    Spacer()
                     
                     Button {
                         Task {
@@ -88,6 +90,6 @@ struct QuoteView: View {
 }
 
 #Preview {
-    QuoteView(show: "Breaking Bad")
+    QuoteView(show: "Better Call Saul")
         .preferredColorScheme(.dark)
 }
